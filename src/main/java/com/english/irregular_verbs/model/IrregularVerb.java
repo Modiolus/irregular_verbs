494eb4f2b3a299e49ab6cf5ac48028c94ac3fb33
Modiolus <tianodessa@gmail.com>
2023-03-16 01:18:11 +0200

    net revision

---
+++ src/main/java/com/english/irregular_verbs/model/IrregularVerb.java
@@ -0,0 +1,15 @@
+package com.english.irregular_verbs.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class IrregularVerb {
+    @Id
+    private int id;
+
+
+
+}
