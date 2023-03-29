47bcca5869efe8ea653dfdf6f5ae3ffa10f5cfda
Modiolus <tianodessa@gmail.com>
2023-03-29 22:23:45 +0300

    index botConfig

--- src/main/java/com/english/irregular_verbs/config/BotConfig.java
+++ src/main/java/com/english/irregular_verbs/config/BotConfig.java
@@ -1,2 +1,47 @@
-package com.english.irregular_verbs.config;public class BotConfig {
+package com.english.irregular_verbs.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("application.properties")
+public class BotConfig {
+
+    @Value("StudyVerb_bot")
+    String botName;
+
+    @Value("${bot.token}")
+    String token;
+
+    @Value("${bot.owner}")
+    Long ownerId;
+
+    public BotConfig() {
+    }
+
+    public String getBotName() {
+        return botName;
+    }
+
+    public void setBotName(String botName) {
+        this.botName = botName;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public Long getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(Long ownerId) {
+        this.ownerId = ownerId;
+    }
+
 }
