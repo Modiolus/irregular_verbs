8ec8b73955309ffa7a5f77321a07424c02766dfb
Modiolus <tianodessa@gmail.com>
2023-04-07 23:20:20 +0300

    create interface

--- src/main/java/com/english/irregular_verbs/service/BotVerbs.java
+++ src/main/java/com/english/irregular_verbs/service/BotVerbs.java
@@ -1,5 +1,23 @@
 package com.english.irregular_verbs.service;
 
-public interface Iverbs {
+import com.english.irregular_verbs.model.IrregularVerb;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+
+import java.util.List;
+
+public interface BotVerbs {
+
+    void suggestionLink(long chatID);
+
+    void donateLink(long chatId);
+
+    void searchVerb(List<IrregularVerb> verbs, String messageText, long chatId);
+
+    void registerUser(Message msg);
+
+    void sendMessage(long chatId, String textToSend);
+
+    void executeMessage(SendMessage message);
 
 }
